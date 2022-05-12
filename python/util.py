2f0d4b49a9b30329c866906ef5faa1ba9b178dfa
avijay96 <63693847+avijay96@users.noreply.github.com>
2022-05-11 21:24:43 -0700

    Updating util with multi-stock (incomplete)

--- python/util.py
+++ python/util.py
@@ -566,3 +566,242 @@ class LSTM_ED_Model():
         plt.legend()
         plt.savefig('../images/ED_Bot_prediction_%d_%d_%d_%d.png' % (
         self.depth, int(self.naive), self.past_history, self.forward_look))
+
+
+class LSTM_Model_copy():
+    def __init__(self,tickerSymbol, start, end,
+                 past_history = 60, forward_look = 1, train_test_split = 0.8, batch_size = 30,
+                 epochs = 50, steps_per_epoch = 200, validation_steps = 50, verbose = 0, infer_train = True,
+                 depth = 1, naive = False, values = 200, plot_values = True, plot_bot = True, tickerSymbolList = None):
+        self.tickerSymbol = tickerSymbol
+        self.start = start
+        self.end = end
+        self.past_history = past_history
+        self.forward_look = forward_look
+        self.train_test_split = train_test_split
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.steps_per_epoch = steps_per_epoch
+        self.validation_steps = validation_steps
+        self.verbose = verbose
+        self.values = values
+        self.depth = depth
+        self.naive = naive
+        self.plot_values = plot_values
+        self.plot_bot = plot_bot
+        self.infer_train = infer_train
+        if tickerSymbolList == None:
+            self.tickerSymbolList = [tickerSymbol]
+        else:
+            self.tickerSymbolList = tickerSymbolList
+
+    def data_preprocess(self, dataset, iStart, iEnd, sHistory, forward_look=1):
+        self.data = []
+        self.target = []
+        iStart += sHistory
+        if iEnd is None:
+            iEnd = len(dataset) - forward_look + 1
+        for i in range(iStart, iEnd):
+            indices = range(i - sHistory, i)  # set the order
+            if forward_look > 1:
+                fwd_ind = range(i, i + forward_look)
+                fwd_entity = np.asarray([])
+                fwd_entity = np.append(fwd_entity, dataset[fwd_ind])
+            reshape_entity = np.asarray([])
+            reshape_entity = np.append(reshape_entity, dataset[
+                indices])  # Comment this out if there are multiple identifiers in the feature vector
+            self.data.append(np.reshape(reshape_entity, (sHistory, 1)))  #
+            if forward_look > 1:
+                self.target.append(np.reshape(fwd_entity, (forward_look, 1)))
+            else:
+                self.target.append(dataset[i])
+        self.data = np.array(self.data)
+        self.target = np.array(self.target)
+
+    def plot_history_values(self):
+        tickerData = yf.Ticker(self.tickerSymbol)
+        tickerDf = yf.download(self.tickerSymbol, start=self.start, end=self.end)
+        tickerDf = tickerDf['Adj Close']
+        data = tickerDf
+        y = data
+        y.index = data.index
+        y.plot()
+        plt.title(f"{self.tickerSymbol}")
+        plt.ylabel("price")
+        plt.show()
+
+    def get_ticker_values(self):
+        self.y_all = []
+        for tickerSymbol in self.tickerSymbolList:
+            tickerData = yf.Ticker(self.tickerSymbol)
+            tickerDf = yf.download(self.tickerSymbol, start=self.start, end=self.end)
+            tickerDf = tickerDf['Adj Close']
+            data = tickerDf
+            self.y_all.append(data.values)
+
+    def prepare_test_train(self):
+        training_size = int(self.y.size * self.train_test_split)
+        training_mean = self.y[:training_size].mean()  # get the average
+        training_std = self.y[:training_size].std()  # std = a measure of how far away individual measurements tend to be from the mean value of a data set.
+        self.y = (self.y - training_mean) / training_std  # prep data, use mean and standard deviation to maintain distribution and ratios
+        self.data_preprocess(self.y, 0, training_size, self.past_history, forward_look = self.forward_look)
+        self.xtrain, self.ytrain = self.data, self.target
+        self.data_preprocess(self.y, training_size, None, self.past_history, forward_look = self.forward_look)
+        self.xtest, self.ytest = self.data, self.target
+
+    def create_p_test_train(self):
+        BATCH_SIZE = self.batch_size
+        BUFFER_SIZE = self.y.size
+        p_train = tf.data.Dataset.from_tensor_slices((self.xtrain, self.ytrain))
+        self.p_train = p_train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True).repeat()
+        p_test = tf.data.Dataset.from_tensor_slices((self.xtest, self.ytest))
+        self.p_test = p_test.batch(BATCH_SIZE).repeat()
+
+    def model_LSTM(self):
+        self.model = tf.keras.models.Sequential()
+        if self.naive:
+            self.model.add(tf.keras.layers.LSTM(20, input_shape = self.xtrain.shape[-2:]))
+        else:
+            self.model.add(tf.keras.layers.LSTM(20, return_sequences=True, input_shape = self.xtrain.shape[-2:]))
+        for i in range(self.depth):
+            self.model.add(tf.keras.layers.LSTM(20, return_sequences=True))
+        if self.naive is False:
+            self.model.add(tf.keras.layers.LSTM(20))
+        self.model.add(tf.keras.layers.Dense(self.forward_look))
+
+        self.model.compile(optimizer='Adam',
+                      loss='mse')
+        self.create_p_test_train()
+        self.model.fit(self.p_train, epochs = self.epochs, steps_per_epoch = self.steps_per_epoch,
+                  validation_data = self.p_test, validation_steps = self.validation_steps,
+                  verbose = self.verbose)
+
+    def infer_values(self, xtest, ytest, ts):
+        self.pred = []
+        self.pred_update = []
+        self.usetest = xtest.copy()
+        if self.infer_train:
+            self.pred_train = []
+            self.pred_update_train = []
+            self.usetest_train = self.xtrain.copy()
+        for i in range(self.values):
+            self.y_pred = self.model.predict(xtest[i,:,:].reshape(1,xtest.shape[1],xtest.shape[2]))[0][:]
+            self.y_pred_update = self.model.predict(self.usetest[i,:,:].reshape(1,xtest.shape[1],xtest.shape[2]))[0][:]
+            self.pred.append(self.y_pred)
+            self.pred_update.append(self.y_pred_update)
+            self.usetest[np.linspace(i+1,i+self.past_history-1,self.past_history-1,dtype=int),np.linspace(self.past_history-2,0,self.past_history-1,dtype=int),:] =  self.y_pred_update[0]
+            if self.infer_train:
+                self.y_pred_train = self.model.predict(self.xtrain[i, :, :].reshape(1, self.xtrain.shape[1], self.xtrain.shape[2]))[0][:]
+                self.y_pred_update_train = \
+                self.model.predict(self.usetest_train[i, :, :].reshape(1, self.xtrain.shape[1], self.xtrain.shape[2]))[0][:]
+                self.pred_train.append(self.y_pred_train)
+                self.pred_update_train.append(self.y_pred_update_train)
+                self.usetest_train[np.linspace(i + 1, i + self.past_history - 1, self.past_history - 1, dtype=int),
+                np.linspace(self.past_history - 2, 0, self.past_history - 1, dtype=int), :] = self.y_pred_update_train[0]
+        self.pred = np.array(self.pred)
+        self.pred_update = np.array(self.pred_update)
+        if self.infer_train:
+            self.pred = np.array(self.pred)
+            self.pred_update = np.array(self.pred_update)
+        if self.forward_look>1:
+            self.RMS_error = (np.mean(((self.ytest[:self.values - 1, 0, 0] - self.pred[1:, 0]) / (self.ytest[:self.values - 1, 0, 0])) ** 2)) ** 0.5
+            self.RMS_error_update = (np.mean(((self.ytest[:self.values - 1, 0, 0] - self.pred_update[1:, 0]) / (
+            self.ytest[:self.values - 1, 0, 0])) ** 2)) ** 0.5
+            if self.infer_train:
+                self.RMS_error_train = (np.mean(((self.ytrain[:self.values - 1, 0, 0] - self.pred_train[1:, 0]) / (
+                self.ytrain[:self.values - 1, 0, 0])) ** 2)) ** 0.5
+                self.RMS_error_update_train = (np.mean(((self.ytrain[:self.values - 1, 0, 0] - self.pred_update_train[1:, 0]) / (
+                    self.ytrain[:self.values - 1, 0, 0])) ** 2)) ** 0.5
+        else:
+            self.RMS_error = (np.mean(((self.ytest[:self.values-1]-self.pred[1:])/(self.ytest[:self.values-1]))**2))**0.5
+            self.RMS_error_update = (np.mean(
+                ((self.ytest[:self.values - 1] - self.pred_update[1:]) / (self.ytest[:self.values - 1])) ** 2)) ** 0.5
+            if self.infer_train:
+                self.RMS_error_train = (np.mean(((self.ytrain[:self.values - 1] - self.pred_train[1:]) / (
+                self.ytrain[:self.values - 1])) ** 2)) ** 0.5
+                self.RMS_error_update_train = (np.mean(((self.ytrain[:self.values - 1] - self.pred_update_train[1:]) / (
+                    self.ytrain[:self.values - 1])) ** 2)) ** 0.5
+
+    def plot_test_values(self):
+        plt.figure()
+        if self.forward_look>1:
+            plt.plot(self.yt[:self.values-1,0,0],label='actual (%s)'%self.ts)
+            plt.plot(self.pred[1:,0],label='predicted (%s)'%self.ts)
+            plt.plot(self.pred_update[1:,0],label='predicted (update)')
+            plt.xlabel("Days")
+            plt.ylabel("Normalized stock price")
+            plt.title('The relative RMS error is %f' % self.RMS_error)
+            plt.legend()
+            plt.savefig('../images/Stock_prediction_%d_%d_%d_%d_%s.png' % (
+            self.depth, int(self.naive), self.past_history, self.forward_look, self.ts))
+            plt.figure()
+            plt.plot(self.pred[1:, 0]-self.pred_update[1:,0], label='difference (%s)' % self.ts)
+            plt.xlabel("Days")
+            plt.ylabel("Prediction difference")
+            plt.savefig('../images/Difference_%d_%d_%d_%d_%s.png' % (
+            self.depth, int(self.naive), self.past_history, self.forward_look, self.ts))
+        else:
+            plt.plot(self.yt[:self.values-1],label='actual (%s)'%self.ts)
+            plt.plot(self.pred[1:],label='predicted (%s)'%self.ts)
+            plt.plot(self.pred_update[1:],label='predicted (update)')
+            plt.xlabel("Days")
+            plt.ylabel("Normalized stock price")
+            plt.title('The relative RMS error is %f' % self.RMS_error)
+            plt.legend()
+            plt.savefig('../images/Stock_prediction_%d_%d_%d_%d_%s.png'%(
+            self.depth,int(self.naive), self.past_history, self.forward_look, self.ts))
+            plt.figure()
+            plt.plot(self.pred[1:] - self.pred_update[1:], label='difference (%s)' % self.ts)
+            plt.xlabel("Days")
+            plt.ylabel("Prediction difference")
+            plt.savefig('../images/Difference_%d_%d_%d_%d_%s.png' % (
+            self.depth, int(self.naive), self.past_history, self.forward_look, self.ts))
+        print('The relative test RMS error is %f'%self.RMS_error)
+        print('The relative test RMS error for the updated dataser is %f' % self.RMS_error_update)
+        if self.infer_train:
+            print('The relative train RMS error is %f' % self.RMS_error_train)
+            print('The relative train RMS error for the updated dataser is %f' % self.RMS_error_update_train)
+
+    def full_workflow(self, model = None):
+        self.get_ticker_values()
+        self.prepare_test_train()
+        self.model_LSTM()
+        if model is None:
+            self.xt = self.xtest
+            self.yt = self.ytest
+            self.ts = self.tickerSymbol
+        else:
+            self.xt = model.xtest
+            self.yt = model.ytest
+            self.ts = model.tickerSymbol
+        self.infer_values(self.xt, self.yt, self.ts)
+
+    def full_workflow_and_plot(self, model = None):
+        self.full_workflow(model = model)
+        self.plot_test_values()
+
+    def plot_bot_decision(self):
+        if self.forward_look > 1:
+            ideal = self.yt[:self.values - 1, 0, 0]
+            pred = np.asarray(self.pred[1:, 0]).reshape(-1,)
+            pred_update = np.asarray(self.pred_update[1:, 0]).reshape(-1,)
+        else:
+            ideal = self.yt[:self.values - 1]
+            pred = np.asarray(self.pred[1:]).reshape(-1,)
+            pred_update = np.asarray(self.pred_update[1:]).reshape(-1,)
+        control_ideal = get_control_vector(ideal)
+        control_pred = get_control_vector(pred)
+        control_pred_update = get_control_vector(pred_update)
+        bot_ideal = buy_and_sell_bot(ideal, control_ideal)
+        bot_pred = buy_and_sell_bot(ideal, control_pred)
+        bot_pred_update = buy_and_sell_bot(ideal, control_pred_update)
+        plt.figure()
+        plt.plot(bot_ideal, label='Ideal case (%.2f)'%bot_ideal[-1])
+        plt.plot(bot_pred, label='From prediction (%.2f)'%bot_pred[-1])
+        plt.plot(bot_pred_update, label='From prediction (updated) (%.2f)'%bot_pred_update[-1])
+        plt.plot(ideal / ideal[0] * 100.0, label='Stock value(%s)' % self.ts)
+        plt.xlabel("Days")
+        plt.ylabel("Percentage growth")
+        plt.legend()
+        plt.savefig('../images/Bot_prediction_%d_%d_%d_%d_%s.png' % (self.depth, int(self.naive), self.past_history, self.forward_look, self.ts))
+
