8c6760be186a2a9505ce9f446bb4fdf6a99940f7
avijay96 <63693847+avijay96@users.noreply.github.com>
2022-05-19 16:12:56 -0700

    Changed py_requirements to include gtab

--- tests/AV_test.py
+++ tests/AV_test.py
@@ -1,1 +1,17 @@
-"Testing GIT PUSH"
+import gtab
+my_path = "../GTAB_banks"
+t = gtab.GTAB(dir_path=my_path)
+
+for i in range(0, 11):
+  # Run for Jan to Jun of one year
+  timeframe_str = str(2010+i) + "-01-01 " + str(2010+i) + "-07-01"
+  try:
+    t.set_active_gtab("google_anchorbank_geo=US_timeframe=" + timeframe_str + ".tsv")
+  except:
+    print('Anchorbank %s not found'%(timeframe_str))
+  # Run for Jul to Dec of one year
+  timeframe_str = str(2010+i) + "-07-01 " + str(2011+i) + "-01-01"
+  try:
+    t.set_active_gtab("google_anchorbank_geo=US_timeframe=" + timeframe_str + ".tsv")
+  except:
+    print('Anchorbank %s not found'%(timeframe_str))
